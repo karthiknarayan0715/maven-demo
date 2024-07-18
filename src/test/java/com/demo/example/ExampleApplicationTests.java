dba0792e4ae2814f3ef73ac56e7655bd581be612
Karthik Narayan V V <karthiknarayan0715@gmail.com>
2024-07-18 15:59:57 +0530

    remove tests

--- src/test/java/com/demo/example/ExampleApplicationTests.java
+++ src/test/java/com/demo/example/ExampleApplicationTests.java
@@ -1,13 +1,13 @@
-package com.demo.example;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ExampleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.demo.example;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ExampleApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
